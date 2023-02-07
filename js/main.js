a69afbf763b338cd1a54c06e14e2827d0089adc9
Kakayou <shigaev_1990@mail.ru>
2023-02-07 19:25:22 +0700

    first look

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-
+Vue.component('todo-list', {
+    props: ['todos'],
+    template: `
+        <div>
+            <div>
+                <p>{{todos.title}}</p>
+                <p>{{todos.tasks}}</p>
+                <button>+</button>
+                <p>{{todos.isDone}}</p>
+            </div>
+        </div>
+    `,
+})
 
 let app = new Vue({
     el: '#app',
-    data: {
-        todo: {
-            title: '123',
-            tasks: ['123', '123'],
-            isDone: true
+    data() {
+        return {
+            todos: {
+                title: '345',
+                tasks: '123',
+                isDone: false
+            },
+            column1: {
+                id: 1,
+                colTodos: []
+            },
+            column2: {
+                id: 2,
+                colTodos: []
+            },
+            column3: {
+                id: 3,
+                colTodos: []
+            },
         }
-    },
-    mounted() {
-        if (localStorage.todo) {
-            this.todo = JSON.parse(localStorage.todo)
-        }
-    },
-    computed: {},
-    methods: {},
+    }
 })
