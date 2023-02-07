5e65283c8c3dd05afc611ae6ce6198ca1fdd1b5f
Kakayou <shigaev_1990@mail.ru>
2023-02-08 00:22:34 +0700

    almost done function adding tasks

--- js/main.js
+++ js/main.js
@@ -3,24 +3,32 @@ Vue.component('todo-list', {
         return {
             taskTitle: 'Example',
             todos: [{
-                tasks: ['do this', 'do ether'],
+                tasks: [],
                 isDone: false
-            }]
+            }],
         };
     },
     methods: {
-        taskAdd() {
-            this.tasks.push()
+        todoDelete() {
+            this.$emit('task-delete');
         }
     },
     template: `
         <div class="todo-card">
             <li v-for="todo in todos" :key="todo.id" class="todo-list">
                 <h2 class="task-title">{{ taskTitle }}</h2>
+                <button class="btn-delete" @click="todoDelete">X</button>
+                
+                <input type="text" placeholder="task" 
+                v-show="!todo.tasks.length" 
+                @keyup.enter="taskAdd()" 
+                v-model="newTask">
+                
                 <div v-for="task in todo.tasks" :key="task.id" class="tasks-list">
                     <p class="task-content">{{ task }}</p>
-                    <input type="checkbox" v-model="todos.isDone" class="is-done">
-                    <button @click="taskAdd" class="btn btn-task-add">+</button>
+                    <input type="checkbox" class="is-done" v-model="task.isDone">
+<!--                    <input type="text" placeholder="task" @keyup.enter="taskAdd()" v-model="newTask">-->
+                    <button class="btn btn-task-add" @click="taskAdd">+</button>
                 </div>
             </li>
         </div>
