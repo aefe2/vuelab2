535fb6017fece5c06a13da88eb7259d30348a073
aefe2 <shigaev_1990@mail.ru>
2023-02-08 14:18:15 +0700

    attemp to create delete func

--- js/main.js
+++ js/main.js
@@ -9,10 +9,15 @@ Vue.component('todo-list', {
     <div class="todo-card">
         <li class="todo-item" v-for="(todo, id) in firstCol" :key="todo.id">
             <p>{{ todo.title }}</p>
-            <p>{{ todo.task }}</p><button>Delete</button>
+            <p>{{ todo.task }}</p><button @click="deleteTask(firstCol)">Delete</button>
         </li>
     </div>
     `,
+    methods: {
+        deleteTask(firstCol) {
+            this.$emit('delete-todo', firstCol)
+        }
+    }
 })
 
 let app = new Vue({
@@ -27,6 +32,10 @@ let app = new Vue({
         thirdCol: []
     },
     methods: {
+        deleteTask(firstCol) {
+            const todoIndex = this.todo.indexOf(firstCol);
+            this.todo.splice(todoIndex, 1);
+        },
         addTask() {
             if (this.todo.title && this.todo.task) {
                 if (this.firstCol.length < 3) {
