18c35ee7138bf6c9761ccc7e150db02c46516bec
aefe2 <shigaev_1990@mail.ru>
2023-02-08 14:05:30 +0700

    Rebuild entire project, adding tasks, 3 columns of tasks

--- js/main.js
+++ js/main.js
@@ -1,62 +1,43 @@
 Vue.component('todo-list', {
-    data() {
-        return {
-            taskTitle: 'Example',
-            todos: [{
-                tasks: [],
-                isDone: false
-            }],
-        };
-    },
-    methods: {
-        todoDelete() {
-            this.$emit('task-delete');
+    props: {
+        firstCol: {
+            type: Array,
+            required: true
         }
     },
     template: `
-        <div class="todo-card">
-            <li v-for="todo in todos" :key="todo.id" class="todo-list">
-                <h2 class="task-title">{{ taskTitle }}</h2>
-                <button class="btn-delete" @click="todoDelete">X</button>
-                
-                <input type="text" placeholder="task" 
-                v-show="!todo.tasks.length" 
-                @keyup.enter="taskAdd()" 
-                v-model="newTask">
-                
-                <div v-for="task in todo.tasks" :key="task.id" class="tasks-list">
-                    <p class="task-content">{{ task }}</p>
-                    <input type="checkbox" class="is-done" v-model="task.isDone">
-<!--                    <input type="text" placeholder="task" @keyup.enter="taskAdd()" v-model="newTask">-->
-                    <button class="btn btn-task-add" @click="taskAdd">+</button>
-                </div>
-            </li>
-        </div>
+    <div class="todo-card">
+        <li class="todo-item" v-for="(todo, id) in firstCol" :key="todo.id">
+            <p>{{ todo.title }}</p>
+            <p>{{ todo.task }}</p><button>Delete</button>
+        </li>
+    </div>
     `,
 })
 
 let app = new Vue({
     el: '#app',
-    data() {
-        return {
-            // taskTitle: '345',
-            // todos: [{
-            //     tasks: ['123'],
-            //     isDone: false
-            // }],
-            column1: {
-                id: 1,
-                colTodos: []
-            },
-            column2: {
-                id: 2,
-                colTodos: []
-            },
-            column3: {
-                id: 3,
-                colTodos: []
-            },
-        }
+    data: {
+        todo: {
+            title: '',
+            task: ''
+        },
+        firstCol: [],
+        secondCol: [],
+        thirdCol: []
     },
-
+    methods: {
+        addTask() {
+            if (this.todo.title && this.todo.task) {
+                if (this.firstCol.length < 3) {
+                    this.firstCol.push({
+                        title: this.todo.title,
+                        task: this.todo.task
+                    })
+                } else alert('Чел...')
+            }
+            this.todo.title = '';
+            this.todo.task = '';
+        },
+    }
 })
